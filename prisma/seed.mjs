d9db4a84953e96b7db6bcdc9d43597c99f8f5481
Jply <josecabeza.compras@gmail.com>
2026-02-08 15:28:57 +0100

    feat: Introduce Prisma ORM setup, defining database models and initial seed data.

--- prisma/seed.mjs
+++ prisma/seed.mjs
@@ -1,21 +1,19 @@
-import users from './data/users.json' with { type: 'json' };
-import products from './data/products.json' with { type: 'json' };
-import productImages from './data/product_images.json' with { type: 'json' };
-import { v2 as cloudinary } from 'cloudinary';
+import { users, products, productImages } from './data.mjs';
+
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+
+const resetDatabase = async () => {
+    console.log("Reiniciando base de datos (TRUNCATE)...");
+    // PostgreSQL
+    await prisma.$executeRaw`TRUNCATE TABLE "users", "products", "product_images" RESTART IDENTITY CASCADE;`;
+}
+
 
 async function main() {
     // PELIGRO: Borramos todo
-    await prisma.productImage.deleteMany();
-    await prisma.product.deleteMany();
-    await prisma.user.deleteMany();
+    await resetDatabase()
 
     console.log("Añadiendo usuarios...")
     await prisma.user.createMany({
@@ -29,35 +27,11 @@ async function main() {
         skipDuplicates: true,
     });
 
-    // console.log("Añadiendo imágenes...")
-    // await prisma.productImage.createMany({
-    //     data: productImages,
-    //     skipDuplicates: true,
-    // });
-
-
-    for (const img of productImages) {
-        try {
-            // 1. Subir a Cloudinary
-            const result = await cloudinary.uploader.upload(`public/products/${img.url}`, {
-                folder: 'shop',
-                use_filename: true,
-                unique_filename: false,
-            });
-
-            // 2. Crear registro en Prisma con la URL resultante
-            await prisma.productImage.create({
-                data: {
-                    url: result.secure_url,
-                    productId: img.productId,
-                },
-            });
-
-            console.log(`✅ Subida: ${img.url}`);
-        } catch (error) {
-            console.error(`❌ Error con ${img.url}:`, error);
-        }
-    }
+    console.log("Añadiendo imágenes...")
+    await prisma.productImage.createMany({
+        data: productImages,
+        skipDuplicates: true,
+    });
 
     console.log("Listo!")
 }
